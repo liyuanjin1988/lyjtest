18eb0f2f855ff3edcc98a7ae2643da75d195bdb7
liyuanjin <1555322@qq.com>
2018-01-29 22:25:24 +0800

    liyuanjin 合并了冲突

--- src/lyjtest/Hql.java
+++ src/lyjtest/Hql.java
@@ -2,7 +2,7 @@ package lyjtest;
 
 public class Hql {
 	public static void main(String[] args) {
-		System.out.println("I'am Jason2");
+		System.out.println("I'am Jason12");
 		System.out.println("I'm liyuanjin");
 		System.out.println("I'am lala");
 	}
