f9e84e70bf61d820e7d27ea5a2ad1fd8c98a4582
jason <jason@qq.com>
2018-01-29 22:23:26 +0800

    jason dev HQL

--- src/lyjtest/Hql.java
+++ src/lyjtest/Hql.java
@@ -2,7 +2,8 @@ package lyjtest;
 
 public class Hql {
 	public static void main(String[] args) {
-		System.out.println("I'am Jason");
+		System.out.println("I'am Jason2");
 		System.out.println("I'm liyuanjin");
+		System.out.println("I'am lala");
 	}
 }
