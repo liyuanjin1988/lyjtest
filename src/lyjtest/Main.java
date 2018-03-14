c0b08b479a0d6ff7d3d6ac5bf1bded131556dd1c
liyuanjin <1555322@qq.com>
2018-03-15 07:43:21 +0800

    呵呵呵

--- src/lyjtest/Main.java
+++ src/lyjtest/Main.java
@@ -2,6 +2,7 @@ package lyjtest;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello jason");
+		System.out.println("Hello jasonAAACCC");
+		System.out.println("HEHE");
 	}
 }
