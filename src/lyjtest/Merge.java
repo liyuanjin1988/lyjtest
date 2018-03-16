cf8cb75b0fe4899afcac7b773df8e8d366ecb5f4
liyuanjin <1555322@qq.com>
2018-03-16 21:05:08 +0800

    add 4 on master branch

--- src/lyjtest/Merge.java
+++ src/lyjtest/Merge.java
@@ -5,6 +5,6 @@ public class Merge {
 		System.out.println("add 1");
 		System.out.println("add 2 on new");
 		System.out.println("add 3 on master branch");
-		
+		System.out.println("add 4 on master branch");
 	}
 }
